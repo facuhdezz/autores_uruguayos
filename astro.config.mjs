3f6a07fa30f4e3e828f34ad0656d466542e66821
FacuHdezz <facuhernandez2000@gmail.com>
2024-04-02 15:25:51 -0300

    corrigiendo astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://facuhdezz.github.io/autores_uruguayos/'
+  site: 'https://facuhdezz.github.io',
+  base: 'autores_uruguayos'
 });
