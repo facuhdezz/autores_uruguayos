d9b905846223534362cb52f53444ee632f0040ea
FacuHdezz <facuhernandez2000@gmail.com>
2024-04-02 20:31:14 -0300

    quitando el build de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://facuhdezz.github.io'
+  integrations: [tailwind(), react()]
 });
--- dist/_astro/Button.Dt-an6zm.js
+++
@@ -1,1 +0,0 @@
-import{B as a}from"./Button.BHXX_ACv.js";import"./index.NEDEFKed.js";export{a as default};
